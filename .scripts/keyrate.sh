a44b13adaab1415e775301dea4090b3ae6bd418e
gabrielzschmitz <gabrielzschmitz@gmail.com>
2020-12-21 04:33:14 -0300

    Keyrate script added

---
+++ .scripts/keyrate.sh
@@ -0,0 +1,11 @@
+#   __ _  ____ ___ 
+#  / _` ||_  // __| GITHUB:https://github.com/gabrielzschmitz               
+# | (_| | / / \__ \ INSTAGRAM:https://www.instagram.com/gabrielz.schmitz/
+#  \__, |/___||___/ DOTFILES:https://github.com/gabrielzschmitz/dotfiles/
+#  |___/    
+#
+#!bin/bash/sh
+
+# Increase the default key speed rate (for nvim)
+xset r rate 300 50
+
