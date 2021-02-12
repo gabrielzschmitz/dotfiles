405005c24ecb2398de5c6fb3d8fa4a4393803974
gabrielzschmitz <gabrielzschmitz@gmail.com>
2021-02-12 16:11:07 -0300

    Screenshot scripts

---
+++ .scripts/fullscreenshot.sh
@@ -0,0 +1,18 @@
+#   __ _  ____ ___
+#  / _` ||_  // __| GITHUB:https://github.com/gabrielzschmitz
+# | (_| | / / \__ \ INSTAGRAM:https://www.instagram.com/gabrielz.schmitz/
+#  \__, |/___||___/ DOTFILES:https://github.com/gabrielzschmitz/dotfiles/
+#  |___/
+#
+#!bin/bash/sh
+
+scrot print-input.png		\
+--quality 100			\
+--exec 'mv $f ~/Pictures/$f'	\
+
+convert ~/Pictures/print-input.png	\
+-crop 1440x900+0+0			\
+~/Pictures/print-output.png		\
+&& rm ~/Pictures/print-input.png	\
+&& mv ~/Pictures/print-output.png ~/Pictures/Fullscreenshot-$(date +%H:%M:%S).png	\
+
---
+++ .scripts/selectivescreenshot.sh
@@ -0,0 +1,15 @@
+#   __ _  ____ ___
+#  / _` ||_  // __| GITHUB:https://github.com/gabrielzschmitz
+# | (_| | / / \__ \ INSTAGRAM:https://www.instagram.com/gabrielz.schmitz/
+#  \__, |/___||___/ DOTFILES:https://github.com/gabrielzschmitz/dotfiles/
+#  |___/
+#
+#!bin/bash/sh
+
+scrot Selectivescreenshot-%H:%M:%S.png           \
+--quality 100                   \
+--select			\
+--freeze			\
+--border			\
+--exec 'mv $f ~/Pictures/$f'    \
+
