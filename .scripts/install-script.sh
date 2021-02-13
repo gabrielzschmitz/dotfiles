1b5a6e3eb312750767a07f4b199aceb9e55193f4
gabrielzschmitz <gabrielzschmitz@gmail.com>
2021-02-13 18:39:41 -0300

    dotfiles bare repository tutorial

--- .scripts/install-script.sh
+++ .scripts/install-script.sh
@@ -1,29 +1,29 @@
 #   __ _  ____ ___ 
-#  / _` ||_  // __| GITHUB:https://github.com/gabrielzschmitz               
+#  / _` ||_  // __| GITHUB:https://github.com/gabrielzschmitz
 # | (_| | / / \__ \ INSTAGRAM:https://www.instagram.com/gabrielz.schmitz/
 #  \__, |/___||___/ DOTFILES:https://github.com/gabrielzschmitz/dotfiles/
 #  |___/    
 #
 #!bin/sh
 
-# Installation Script for Manjaro I3 Minimal
+# Installation Script for Artix Base Install
 # by gabrielzschmitz(https://github.com/gabrielzschmitz)
 # Before running the script install yay manually
 
 # Remove Programs
-pacman -Rsnu --noconfirm cmus;
-pacman -Rsnu --noconfirm mousepad;
-pacman -Rsnu --noconfirm bmenu;
-pacman -Rsnu --noconfirm conky;
-pacman -Rsnu --noconfirm conky-i3;
-pacman -Rsnu --noconfirm i3-default-artwork;
-pacman -Rsnu --noconfirm manjaro-zsh-config;
-pacman -Rsnu --noconfirm manjaro-browser-settings;
-pacman -Rsnu --noconfirm morc-menu;
-pacman -Rsnu --noconfirm palemoon-bin;
-pacman -Rsnu --noconfirm pacui;
-pacman -Rsnu --noconfirm nitrogen;
-pacman -Rsnu --noconfirm firefox;
+# pacman -Rsnu --noconfirm cmus;
+# pacman -Rsnu --noconfirm mousepad;
+# pacman -Rsnu --noconfirm bmenu;
+# pacman -Rsnu --noconfirm conky;
+# pacman -Rsnu --noconfirm conky-i3;
+# pacman -Rsnu --noconfirm i3-default-artwork;
+# pacman -Rsnu --noconfirm manjaro-zsh-config;
+# pacman -Rsnu --noconfirm manjaro-browser-settings;
+# pacman -Rsnu --noconfirm morc-menu;
+# pacman -Rsnu --noconfirm palemoon-bin;
+# pacman -Rsnu --noconfirm pacui;
+# pacman -Rsnu --noconfirm nitrogen;
+# pacman -Rsnu --noconfirm firefox;
 
 # Enable AUR and Update Mirrors
 sudo -u	gabrielzschmitz \
@@ -57,7 +57,7 @@ discord;		\
 # Make Steam close to system tray
 echo "export STEAM_FRAME_FORCE_CLOSE=1" >> .profile
 
-# GIT
+# Git
 # Set git to use the credential memory cache
 git config --global credential.helper cache;
 # Set the cache to timeout after 1 hour (setting is in seconds)
@@ -68,4 +68,13 @@ git config --global credential.helper 'cache --timeout=3600';
 # DarkCloud
 # User Agent Plataform Spoofer
 # UBlock Origin
+
+# Dotfiles
+# echo "alias dots='/usr/bin/git --git-dir=$HOME/Documents/dotfiles --work-tree=/home/gabrielzschmitz'" >> $HOME/.zshrc
+# source ~/.zshrc
+# echo "$HOME/Documents/dotfiles" >> .gitignore
+# git clone --bare https://www.github.com/gabrielzschmitz/dotfiles.git $HOME/Documents/dotfiles
+# ('rm' and 'rm -rf' all the files/folders that are making it impossible to checkout)
+# dots checkout
+# dots config --local status.showUntrackedFiles no
 
