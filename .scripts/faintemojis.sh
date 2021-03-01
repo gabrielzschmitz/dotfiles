ea5be31c3102c764557f18b2b0339213c34c814d
gabrielzschmitz <gabrielzschmitz@gmail.com>
2021-02-28 21:14:24 -0300

    Little fix

--- .scripts/faintemojis.sh
+++ .scripts/faintemojis.sh
@@ -1,6 +1,12 @@
+#   __ _  ____ ___ 
+#  / _` ||_  // __| GITHUB:https://github.com/gabrielzschmitz               
+# | (_| | / / \__ \ INSTAGRAM:https://www.instagram.com/gabrielz.schmitz/
+#  \__, |/___||___/ DOTFILES:https://github.com/gabrielzschmitz/dotfiles/
+#  |___/    
+#
 #!/bin/sh
 
-# The famous "get a menu of emojis to copy" script.
+# The famous "get a menu of emojis to copy" script by Luke Smith (just added the theme).
 
 # Get user selection via dmenu from emoji file.
 chosen=$(cut -d ';' -f1 ~/.scripts/emojis | dmenu -nf '#6f798c' -nb '#232731' -sb '#3b8563' -sf '#9faab8' -fn 'FiraCode Nerd Font-12' -p 'emojis' -i -l 15 | sed "s/ .*//")
@@ -16,3 +22,4 @@ else
 	echo "$chosen" | tr -d '\n' | xclip -selection clipboard
 	notify-send -t 500 "'$chosen' copied to clipboard." &
 fi
+
