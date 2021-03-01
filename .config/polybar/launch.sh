8e72f382209f44d2e03919b6760f3b8a025f6f84
gabrielzschmitz <gabrielzschmitz@gmail.com>
2021-02-28 21:41:30 -0300

    Polybar added

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar"
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+
+#polybar -qr main -c "$DIR"/config.ini &
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar -qr main -c "$DIR"/config.ini &
+done
+
