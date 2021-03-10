88d6c522478cde4d1b2066e657ee14a966295e5f
gabrielzschmitz <gabrielzschmitz@gmail.com>
2021-03-10 08:21:17 -0300

    dmenu changed

--- .scripts/faintemojis.sh
+++ .scripts/faintemojis.sh
@@ -9,7 +9,7 @@
 # The famous "get a menu of emojis to copy" script by Luke Smith (just added the theme).
 
 # Get user selection via dmenu from emoji file.
-chosen=$(cut -d ';' -f1 ~/.scripts/emojis | dmenu -nf '#6f798c' -nb '#232731' -sb '#3b8563' -sf '#9faab8' -fn 'FiraCode Nerd Font-12' -p 'emojis' -i -l 15 | sed "s/ .*//")
+chosen=$(cut -d ';' -f1 ~/.scripts/emojis | dmenu -p 'emojis' -l 15 | sed "s/ .*//")
 
 # Exit if none chosen.
 [ -z "$chosen" ] && exit
