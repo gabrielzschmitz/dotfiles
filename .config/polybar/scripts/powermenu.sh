224764709d47c090be4385fe19287d3e549bee2a
gabrielzschmitz <gabrielzschmitz@gmail.com>
2021-03-20 07:53:13 -0300

    Update Config

--- .config/polybar/scripts/powermenu.sh
+++ .config/polybar/scripts/powermenu.sh
@@ -11,11 +11,11 @@ uptime=$(uptime -p | sed -e 's/up //g')
 rofi_command="rofi -theme $dir/powermenu.rasi"
 
 # Options
-shutdown=" Shutdown"
-reboot=" Restart"
-lock=" Lock"
-suspend=" Sleep"
-logout=" Logout"
+shutdown="⏻ Shutdown"
+reboot=" Restart"
+lock=" Lock"
+suspend="⏾ Sleep"
+logout=" Logout"
 
 # Confirmation
 confirm_exit() {
