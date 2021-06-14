4110b682673f5bb02163789ed01c87e51475fa59
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2021-06-14 00:24:15 -0300

    zerofetch added

---
+++ .config/zerofetch/arch.h
@@ -0,0 +1,22 @@
+// This was stolen from neofetch
+char *LOGO[] = {
+    "                  -`                    ",
+    "                 .o+`                   ",
+    "                `ooo/                   ",
+    "               `+oooo:                  ",
+    "              `+oooooo:                 ",
+    "              -+oooooo+:                ",
+    "            `/:-:++oooo+:               ",
+    "           `/++++/+++++++:              ",
+    "          `/++++++++++++++:             ",
+    "         `/+++ooooooooooooo/`           ",
+    "        ./ooosssso++osssssso+`          ",
+    "       .oossssso-````/ossssss+`         ",
+    "      -osssssso.      :ssssssso.        ",
+    "     :osssssss/        osssso+++.       ",
+    "    /ossssssss/        +ssssooo/-       ",
+    "  `/ossssso+/:-        -:/+osssso+-     ",
+    " `+sso+:-`                 `.-/+oso:    ",
+    "`++:.                           `-/+/   ",
+    ".`                                 `/   "
+};
