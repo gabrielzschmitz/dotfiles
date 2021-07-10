ff4fc96666c0a58e19d9a12a344d2bea43e629d0
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2021-07-10 19:27:34 -0300

    alpha remove

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,21 +8,17 @@
 # GITHUB:https://github.com/gabrielzschmitz		  
 # INSTAGRAM:https://www.instagram.com/gabrielz.schmitz/   
 # DOTFILES:https://github.com/gabrielzschmitz/dotfiles/   
-
 #!/bash/sh
-
 # Add this script to your wm startup file.
-
 DIR="$HOME/.config/polybar"
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+sleep 1
 
 # Launch the bar
-#polybar -qr main -c "$DIR"/config.ini &
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar -qr main -c "$DIR"/config.ini &
 done
