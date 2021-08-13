bdd0bed978e3e05fc289acc26a612c1ce51b5d2b
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2021-08-13 09:03:00 -0300

    dmenu fix

--- .config/zerodmenu/config.h
+++ .config/zerodmenu/config.h
@@ -6,7 +6,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const char *fonts[] = {
 	"FiraCode Nerd Font:size=12:antialias=true:autohint=true"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#9096ae", "#20232f" },
