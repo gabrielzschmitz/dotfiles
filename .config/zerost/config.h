836d6ed30030b7fc8076aabea5322645f287a98f
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2021-08-13 07:12:50 -0300

    remove borders

--- .config/zerost/config.h
+++ .config/zerost/config.h
@@ -7,7 +7,7 @@
  */
 static char *font = "FiraCode Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
 static char *font2[] = { "FiraCode Nerd Font:pixelsize=10:antialias=true:autohint=true" };
-static int borderpx = 2;
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
