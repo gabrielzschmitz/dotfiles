a8b0afe6ecf194480c823acfc72e21bc40f76613
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2021-08-15 09:42:02 -0300

    zero colors added by default

--- .config/zerost/config.h
+++ .config/zerost/config.h
@@ -113,28 +113,28 @@ float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
+	"#20232f", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	"#806382",
+	"#436668",
+	"#8c8089",
+	"#4f6589",
+	"#8f6b8f",
+	"#446781",
+	"#8c8089",
+	"#9d8f92",
+	"#8f6e8d",
+	"#59729a",
+	"#9d8f92",
 	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
+	"#9f7699",
+	"#4d7694",
+	"#9096ae",
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
-	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
-	"#ebdbb2", /* 259 -> fg */
+	"#a2a6ba", /* 256 -> cursor */
+	"#35394a", /* 257 -> rev cursor*/
+	"#20232f", /* 258 -> bg */
+	"#9096ae", /* 259 -> fg */
 };
 
 
