c04df517c1584a9af3148a77d79bdf0066f95c63
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2021-09-18 08:49:41 -0300

    logos cleanup

--- .local/src/zerofetch/artix.h
+++ .local/src/zerofetch/artix.h
@@ -1,23 +1,10 @@
-// This was stolen from neofetch
+// This was stolen from pfetch
 char *LOGO[] = {
-    "                   '		        ",
-    "                  'o'		        ",
-    "                 'ooo'		        ",
-    "                'ooxoo'		        ",
-    "               'ooxxxoo'		",
-    "              'oookkxxoo'		",
-    "             'oiioxkkxxoo'		",
-    "            '.;:iiiioxxxoo'	        ",
-    "               `'.;::ioxxoo'	        ",
-    "          ';.       `';:iooo'	        ",
-    "         'ooooo:;.      `':io'	        ",
-    "        'ooooxxxxooi:;.     .;'	        ",
-    "       'ooooxxxxxkkxooooi:.    `           ",
-    "      'ooooxxxxxkkkkxoiiiiii:'	        ",
-    "     'ooooxxxxxkxxoiiii:;'`    .i'	        ",
-    "    'ooooxxxxxoi:::'``      .;ioxo'        ",
-    "   'ooooxooi:;' `        .:iiixkxxo'       ",
-    "  'ooooi:'^`               `':iioxxo'      ",
-    " 'jiv'`                         `';jo'     ",
-    "'`                                   `'    "
+    "      /\\       ",
+    "     /  \\      ",
+    "    /`'.,\\     ",
+    "   /     ',    ",
+    "  /      ,`\\   ",
+    " /   ,.'`.  \\  ",
+    "/.,'`     `'.\\ "
 };
--- .local/src/zerofetch/config.h
+++ .local/src/zerofetch/config.h
@@ -1,12 +1,12 @@
 // Change the ASCII Art
-#include "partix.h"
-// 93 yellow; 94 blue; 96 cyan; 95 magenta; 97 white
+#include "artix.h"
+// 90 black; 91 red; 92 green; 93 yellow; 94 blue; 95 magenta; 96 cyan; 97 white
 // Define the ASCII color
-#define COLORA "\e[1;96m"
-// Define the Title color
-#define COLORT "\e[1;97m"
+#define COLORA "\e[1;94m"
 // Define the Functions colors
 #define COLOR "\e[1;96m"
+// Define the Title color
+#define COLORT "\e[1;97m"
 
 #define CONFIG \
 { \
