0ae51f24a896ff2d185a5a195f8895b6396de8d5
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2022-07-23 00:57:16 -0300

    adding monocle keybind

--- .local/src/zerodwm/config.h
+++ .local/src/zerodwm/config.h
@@ -178,6 +178,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ Mod1Mask,                     XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[4]} },
