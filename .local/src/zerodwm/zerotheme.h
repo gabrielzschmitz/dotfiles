9f19d4e87f92d8b52356155ad185fdfbed137527
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2022-07-26 20:16:06 -0300

    better empty tag color

--- .local/src/zerodwm/zerotheme.h
+++ .local/src/zerodwm/zerotheme.h
@@ -1,7 +1,7 @@
 static const char black[]       = "#080808";
 static const char white[]       = "#b3b3b3";
 static const char gray[]        = "#e5e5e5";
-static const char gray2[]       = "#1a1a1a";
+static const char gray2[]       = "#262626";
 static const char gray3[]       = "#333333";
 static const char blue[]        = "#3f5273";
 static const char cyan[]        = "#324f66";
